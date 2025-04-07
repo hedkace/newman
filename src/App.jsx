9e8c18d69487c87d4d8385e9f829b2444f9d66ea
aka: cauzality, zeno evil, ... <cauzality@comcast.net>
2025-04-07 10:07:28 -0500

    readying for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import CreatePage from './pages/CreatePage'
 import PostPage from './pages/PostPage'
 
 //axios.defaults.baseURL = "http://localhost:4000"
-axios.defaults.baseURL = "https://newman-server.vercel.app/"
+axios.defaults.baseURL = "https://newman-server.vercel.app"
 axios.defaults.withCredentials = true
 
 function App() {
